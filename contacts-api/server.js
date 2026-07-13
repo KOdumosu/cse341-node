0b9aabd656080dcf5aabc33c5aece29e2cf3dff7
Kehinde Olusegun Odumosu <odumosukenny@gmail.com>
2026-07-13 10:45:47 +0100

    Add Swagger setup

--- contacts-api/server.js
+++ contacts-api/server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
-
 const express = require("express");
 const { initDb } = require("./data/database");
-
+const { swaggerUi, swaggerSpec } = require("./swagger");
 const contactsRoutes = require("./routes/contacts");
 
 const app = express();
@@ -10,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/", (req, res) => {
   res.send("Contacts API is running!");
