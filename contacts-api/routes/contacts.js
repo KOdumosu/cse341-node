925d0fd02f71e2f07cf6727acd72b9b0891864f6
Kehinde Olusegun Odumosu <odumosukenny@gmail.com>
2026-07-13 21:35:15 +0100

    Add Swagger route documentation

--- contacts-api/routes/contacts.js
+++ contacts-api/routes/contacts.js
@@ -4,15 +4,133 @@ const router = express.Router();
 
 const contactsController = require("../controllers/contacts");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Contacts
+ *   description: Contacts Management API
+ */
 
+/**
+ * @swagger
+ * /contacts:
+ *   get:
+ *     summary: Get all contacts
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: Returns all contacts
+ */
 router.get("/", contactsController.getAll);
 
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   get:
+ *     summary: Get a contact by ID
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: MongoDB ObjectId
+ *     responses:
+ *       200:
+ *         description: Contact found
+ *       404:
+ *         description: Contact not found
+ */
 router.get("/:id", contactsController.getSingle);
 
+/**
+ * @swagger
+ * /contacts:
+ *   post:
+ *     summary: Create a new contact
+ *     tags: [Contacts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - email
+ *               - favoriteColor
+ *               - birthday
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               favoriteColor:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *                 example: "2000-01-01"
+ *     responses:
+ *       201:
+ *         description: Contact created successfully
+ */
 router.post("/", contactsController.createContact);
 
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   put:
+ *     summary: Update a contact
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               favoriteColor:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: Contact updated successfully
+ */
 router.put("/:id", contactsController.updateContact);
 
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   delete:
+ *     summary: Delete a contact
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Contact deleted successfully
+ */
 router.delete("/:id", contactsController.deleteContact);
 
 module.exports = router;
